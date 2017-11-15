3aff45a08342daca4c9beb356ef618453f289225
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-15 13:37:17 -0500

    Updated from Brython Server: 11/15/2017 1:37:16 PM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,16 @@
+#Pedro Gallino
+#11/15/17
+#dictionaryDemo.py - more list practice
+
+dictionary = ['alphabet','sweatshirt','sweatpants','shorts','computer','waterbottle']
+
+dictionary.sort()
+
+number = int(input('What number word do you want to look up'))
+
+if number>=len(dictionary):
+    print(number,'is an invalid choice')
+    
+else:
+    print('Word number',number,'is',dictionary[number-1])
 

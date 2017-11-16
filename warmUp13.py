3bef5c7602737c6a7f6b8200a76cafc315dfe4e5
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-16 14:20:21 -0500

    Updated from Brython Server: 11/16/2017 2:20:20 PM

--- warmUp13.py
+++ warmUp13.py
@@ -1,1 +1,15 @@
+#Pedro Gallino
+#11/16/17
+#warmUp13.py - prints the min max sum of a list of random numbers
+
+nums = []
+from random import randint
+for k in range(1,21):
+    i = randint(-100,100)
+    nums.append(i)
+
+nums.sort()
+print('The max is',nums[-1])
+print('The minimum is',nums[0])
+print('The sum is',sum(nums))
 

f72b2c92fe1904ad56fde51eb8a62928490b2d57
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-16 14:32:37 -0500

    Updated from Brython Server: 11/16/2017 2:32:36 PM

--- displayDate.py
+++ displayDate.py
@@ -2,13 +2,14 @@
 #11/15/17
 #displayDate.py - displays the date, day of the week, month, and year
 
+import datetime 
 
 weekdays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 months = ['January','February','March','April','May','June','July','August','September','October','November','December' ]
 today = datetime.date.today()
-date = today.date
+num = today.day
 month = today.month
 year = today.year
 day = today.weekday()
-print('Today is',weekdays[day],',',months[month],date,year)
+print('Today is',weekdays[day],',',months[month-1],num,year)
 

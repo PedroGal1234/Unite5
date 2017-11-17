aacbbc20447b55100daa9ec38bffe80cc0ff8897
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-17 14:36:20 -0500

    Updated from Brython Server: 11/17/2017 2:36:20 PM

--- warmUp14.py
+++ warmUp14.py
@@ -1,1 +1,6 @@
+#Pedro Gallino
+#11/17/17
+#warmUp14.py - what is the users name and then say the users initials
 
+x = input('Enter you full name').split(' ')
+print('Your initials are: ',x[0][0]+x[1][0])

a0f24159e9961b872a450d6ad288c4a34dac71fa
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-20 14:55:19 -0500

    Updated from Brython Server: 11/20/2017 2:55:18 PM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,20 @@
+#Pedro Gallino
+#11/20/17
+#toDoList.py - Lets people write a to do list
 
+print('Valid Commands Are: add, delete, print, quit')
+toDo = []
+while True:
+    thing = str(input('>'))
+    if thing == 'quit':
+        break
+    elif thing == 'print':
+        print(toDo)
+        for i in toDo:
+            print(i)
+    elif thing[0:2] == 'add':
+        toDo.append(thing[3:])
+    elif thing[0:5] == 'delete':
+        toDo.remove[thing[3:]]
+        
+    

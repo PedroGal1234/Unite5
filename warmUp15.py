ef11b6f2fd05975374f146afe3e336697c30836c
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-30 14:16:18 -0500

    Updated from Brython Server: 11/30/2017 2:16:16 PM

--- warmUp15.py
+++ warmUp15.py
@@ -1,1 +1,16 @@
+#Pedro Gallino
+#11/30/17
+#warmUp15.py - inputs a list and then doubles all the numbers
+
+numbers = []
+
+def double(L):
+    for i in (L):
+        k = i*2
+        numbers.append(k)
+    return numbers
+
+print(double([98,100,6]))
+
+
 

1f264eb3b95e91befe0f2c2b03c529944fe4196c
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-11-30 14:41:00 -0500

    Updated from Brython Server: 11/30/2017 2:40:59 PM

--- sort.py
+++ sort.py
@@ -7,36 +7,6 @@ from time import time
 
 N = 10000 #how many numbers will be sorted
 
-"""
-/* a[0] to a[n-1] is the array to sort */
- 2 int i,j;
- 3 int n;
- 4 
- 5 /* advance the position through the entire array */
- 6 /*   (could do j < n-1 because single element is also min element) */
- 7 for (j = 0; j < n-1; j++)
- 8 {
- 9     /* find the min element in the unsorted a[j .. n-1] */
-10 
-11     /* assume the min is the first element */
-12     int iMin = j;
-13     /* test against elements after j to find the smallest */
-14     for (i = j+1; i < n; i++)
-15     {
-16         /* if this element is less, then it is the new minimum */
-17         if (a[i] < a[iMin])
-18         {
-19             /* found new minimum; remember its index */
-20             iMin = i;
-21         }
-22     }
-23 
-24     if (iMin != j) 
-25     {
-26         swap(a[j], a[iMin]);
-27     }
-28 }
-"""
 def mySort(A):
     n = len(A)
     for j in range(0,n-1,1) : 
